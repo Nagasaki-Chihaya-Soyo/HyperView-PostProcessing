30f0dc3bcff835b9dfa2a7fba1d780b8c22a3e47
Soyo Nagasaki <125942937+Nagasaki-Chihaya-Soyo@users.noreply.github.com>
2026-02-04 10:19:21 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+import sys
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+from gui.app import main
+if __name__ == "__main__":
+    main()
